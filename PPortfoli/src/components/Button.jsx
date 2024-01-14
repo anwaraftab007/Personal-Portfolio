8f7d774492d872eafdf77f25a5ffbe6bd388a94b
anwaraftab007 <anwaraftab007@gmail.com>
2024-01-14 18:25:26 +0530

    [001] Project Hero Section under Construction

---
+++ PPortfoli/src/components/Button.jsx
@@ -0,0 +1,17 @@
+const Button = ({label, iconUrl,  backgroundColor, borderColor, textColor, link}) => {
+  return (
+    <div className="my-3">
+        <a href={link} className="">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-monsterrat text-lg leading-none rounded-full
+        ${
+            backgroundColor? `${backgroundColor} ${textColor} ${borderColor} hover:bg-dark-yellow hover:text-black` : "bg-dark-yellow border-[#fdb82c] text-black hover:text-[#fdb82c] hover:bg-[#2d2d2d]"
+        }`}>
+        <span className="text-xl">{label}</span> {iconUrl && <img src={iconUrl}/>}
+    </button>
+    </a> 
+    </div>
+   
+  )
+}
+
+export default Button
--- PPortfoli/src/sections/Hero.jsx
+++ PPortfoli/src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import hamburger from "../assets/icons/hamburger.svg"
+import arrow from "../assets/icons/arrow-right.svg"
+import Button from "../components/Button"
 import { useState } from 'react'
 
 const Hero = () => {
@@ -18,7 +20,23 @@ const Hero = () => {
         <div className="w-10 h-10 bg-twitter bg-cover max-lg:mx-2 max-sm:mx-1"></div>
        </div>
       </div>
-      <div className="min-h-screen w-full bg-[#2d2d2d]">
+      <div className="flex md:min-h-screen w-full bg-[#2d2d2d] lg:flex-col flex-row justify-between items-center">
+        <div className="relative lg:w-3/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-dark-yellow">Learning Web Design</p>
+        <h1 className="mt-10 font-palanquin text-8xl font-bold text-white">
+          I'm <span className="text-dark-yellow inline-block lg:mt-3">Anwar</span> Aftab
+        </h1>
+        <p className="font-monsterrat text-lg text-wrap leading-8 mt-6 mb-14 sm:max-w-sm text-white">
+          
+        </p>
+        <div className="flex max-sm:flex-col flex-row md:gap-7 gap-4">
+        <Button label='About Me' link="#about-me" iconUrl={arrow}/>
+        <Button label='Resume'
+        link="#resume" backgroundColor="bg-[#2d2d2d]"
+        borderColor="border-[#fdb82c] "
+        textColor="text-[#fdb82c]"/>
+        </div>
+        </div>
       </div>
     </section>
   )
